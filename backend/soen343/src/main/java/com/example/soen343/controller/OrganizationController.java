f4642b7deb3795656b3c82571575024b5b551c31
Youssef Ouakaa <youssef.ouakaa2@gmail.com>
2025-04-08 01:01:54 -0400

    Added get for organizations

--- backend/soen343/src/main/java/com/example/soen343/controller/OrganizationController.java
+++ backend/soen343/src/main/java/com/example/soen343/controller/OrganizationController.java
@@ -1,2 +1,22 @@
-package com.example.soen343.controller;public class OrganizationController {
+package com.example.soen343.controller;
+
+import com.example.soen343.model.Organization;
+import com.example.soen343.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/organizations")
+@CrossOrigin(origins = "http://localhost:3000")
+public class OrganizationController {
+
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @GetMapping
+    public List<Organization> getAllOrganizations() {
+        return organizationRepository.findAll();
+    }
 }
